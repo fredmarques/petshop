eeb260726de5ce670f08531a56c02940befcc9fe
Frederico Marques <frederico.marques.22@gmail.com>
2017-06-01 22:52:12 -0300

    added calendar component

--- src/components/routes/calendar/Calendar.js
+++ src/components/routes/calendar/Calendar.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import './react-big-calendar.css'
+import BigCalendar from 'react-big-calendar';
+import moment from 'moment';
 
+
+// Setup the localizer by providing the moment (or globalize) Object
+// to the correct localizer.
+BigCalendar.momentLocalizer(moment); // or globalizeLocalizer
+const myEventsList = [];
+const MyCalendar = props => (
+  <div>
+    <BigCalendar
+      events={myEventsList}
+      startAccessor={new Date(2014, 0, 1)}
+      endAccessor={new Date(2050, 0, 1)}
+    />
+  </div>
+);
 class Calendar extends Component {
     render() {
         return (
             <div>
-                
+               <MyCalendar/>
             </div>
         );
     }
