c9e60401c7c7af93cd43da81dc492864d10c55c2
Bruno Fabbri <fabbri07@gmail.com>
2017-06-11 20:50:14 -0300

    New home

--- src/components/routes/home/Home.js
+++ src/components/routes/home/Home.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+import { Image, Media } from 'react-bootstrap';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import dog from '../../../assets/dog.jpg';
+import cat from '../../../assets/cat.jpeg';
+import tiger from '../../../assets/tiger.jpeg';
 import Card from '../../shell/items/card';
+import './Home.css';
 
 const body = 'Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.';
 const heading = 'Dog fofo';
@@ -10,18 +14,19 @@ class Home extends Component {
     render() {
         return (
             <Grid fluid>
-                <Row> 
-                    <Col xs={6} md={4} lg={4}>
-                        <Card width={64} height={64} image={dog} alt={'dog'} heading={heading} body={body}/>
+                <Row>
+                    <Col xs={12} sm={12} md={12} lg={12}>
+                        <h1 className="welcome-title">Bem vindo ao Petcare</h1>
+                        <h3 className="welcome-subtitle">Aqui é o lar do seu bichinho de estimação</h3>
+                    </Col> 
+                    <Col xs={12} sm={4} md={4} lg={4}>
+                        <Image className="animalImage" width={200} height={150} src={dog} alt={'dog'} rounded />
                     </Col>
-                    <Col xs={6} md={4} lg={8}>
-                        <Card width={64} height={64} image={dog} alt={'dog'} heading={heading} body={body}/>
+                    <Col xs={12} sm={4} md={4} lg={4}>
+                        <Image className="animalImage" width={200} height={150} src={cat} alt={'cat'} rounded />
                     </Col>
-                    <Col xs={6} md={4} lg={4}>
-                        <Card width={64} height={64} image={dog} alt={'dog'} heading={heading} body={body}/>
-                    </Col>
-                    <Col xs={6} md={8} lg={8}>
-                        <Card width={64} height={64} image={dog} alt={'dog'} heading={heading} body={body}/>
+                    <Col xs={12} sm={4} md={4} lg={4}>
+                        <Image className="animalImage" width={200} height={150} src={tiger} alt={'tiger'} rounded />
                     </Col>
                </Row>
             </Grid>
