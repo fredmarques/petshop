4076d3b072050f567e796a24c4791dc474c089f8
Frederico Marques <frederico.marques.22@gmail.com>
2017-06-11 18:44:03 -0300

    all products enabled

--- src/api/products.js
+++ src/api/products.js
@@ -10,35 +10,35 @@ export default [
     "height": 256,
     "heading": "Dog bonitinho para ficar de exemplo!",
     "body": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum harum dolorem velit quasi vitae vero nostrum dolores rem dignissimos. Doloremque harum culpa qui dolores, corporis hic, sint sed fugit provident."
+},
+{
+    "id":2, 
+    "inventory": 100,
+    "image": dog,
+    "alt": "dog",
+    "width": 256,
+    "height": 256,
+    "heading": "Dog bonitinho para ficar de exemplo!",
+    "body": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum harum dolorem velit quasi vitae vero nostrum dolores rem dignissimos. Doloremque harum culpa qui dolores, corporis hic, sint sed fugit provident."
+},
+{
+    "id":3, 
+    "inventory": 100,
+    "image": dog,
+    "alt": "dog",
+    "width": 256,
+    "height": 256,
+    "heading": "Dog bonitinho para ficar de exemplo!",
+    "body": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum harum dolorem velit quasi vitae vero nostrum dolores rem dignissimos. Doloremque harum culpa qui dolores, corporis hic, sint sed fugit provident."
+},
+{
+    "id":4, 
+    "inventory": 100,
+    "image": dog,
+    "alt": "dog",
+    "width": 256,
+    "height": 256,
+    "heading": "Dog bonitinho para ficar de exemplo!",
+    "body": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum harum dolorem velit quasi vitae vero nostrum dolores rem dignissimos. Doloremque harum culpa qui dolores, corporis hic, sint sed fugit provident."
 }
 ];
-// {
-//     "id":2, 
-//     "inventory": 100,
-//     "image": dog,
-//     "alt": "dog",
-//     "width": 256,
-//     "height": 256,
-//     "heading": "Dog bonitinho para ficar de exemplo!",
-//     "body": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum harum dolorem velit quasi vitae vero nostrum dolores rem dignissimos. Doloremque harum culpa qui dolores, corporis hic, sint sed fugit provident."
-// },
-// {
-//     "id":3, 
-//     "inventory": 100,
-//     "image": dog,
-//     "alt": "dog",
-//     "width": 256,
-//     "height": 256,
-//     "heading": "Dog bonitinho para ficar de exemplo!",
-//     "body": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum harum dolorem velit quasi vitae vero nostrum dolores rem dignissimos. Doloremque harum culpa qui dolores, corporis hic, sint sed fugit provident."
-// },
-// {
-//     "id":4, 
-//     "inventory": 100,
-//     "image": dog,
-//     "alt": "dog",
-//     "width": 256,
-//     "height": 256,
-//     "heading": "Dog bonitinho para ficar de exemplo!",
-//     "body": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum harum dolorem velit quasi vitae vero nostrum dolores rem dignissimos. Doloremque harum culpa qui dolores, corporis hic, sint sed fugit provident."
-// }
