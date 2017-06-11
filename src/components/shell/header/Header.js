1993671822fbdb3758f0dae45c5551ee8ab30267
Bruno Fabbri <fabbri07@gmail.com>
2017-06-11 19:45:56 -0300

    Added the name "PetCare" to the header

--- src/components/shell/header/Header.js
+++ src/components/shell/header/Header.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import logo from '../../../logo.svg';
 
 class Header extends Component {
-    render() {
-        return (
-            <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" /> 
+        <span style={{fontSize: 30, color: "#42cef4"}}>PetCare</span>
+      </div>
+    );
+  }
 }
 
 export default Header;
