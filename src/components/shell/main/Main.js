4ecf6c33da2b9f2cdc7afb86826f569205e5194c
Bruno Fabbri <fabbri07@gmail.com>
2017-06-11 18:06:34 -0300

    Main code indentation

--- src/components/shell/main/Main.js
+++ src/components/shell/main/Main.js
@@ -3,16 +3,16 @@ import Content from '../content/Content';
 import Navigation from '../navigation/Navigation';
 import { BrowserRouter}  from 'react-router-dom';
 class Main extends Component {
-    render() {
-        return (
-       <BrowserRouter>
-        <div>
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="mainContent">
           <Navigation />
           <Content/>
         </div>
-       </BrowserRouter> 
-        );
-    }
+      </BrowserRouter> 
+    );
+  }
 }
 
 export default Main;
