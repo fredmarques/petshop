c2ec2f88346ad865ccc049cde514f756fff6b783
Frederico Marques <frederico.marques.22@gmail.com>
2017-06-11 17:41:32 -0300

    remove unnecessary js evaluation from jsx

--- src/components/shell/navigation/Navigation.js
+++ src/components/shell/navigation/Navigation.js
@@ -3,47 +3,51 @@ import { Nav, NavItem } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
 export default class TitleBar extends Component {
-    constructor(props) {
-        super(props);
-        // this.state = { selectedKey: 1 };
-        this.handleSelect = this.handleSelect.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    // this.state = { selectedKey: 1 };
+    this.handleSelect = this.handleSelect.bind(this);
+  }
 
-    handleSelect(eventKey) {
-        this.setState({selectedKey: eventKey});
-    }
+  handleSelect(eventKey) {
+    this.setState({ selectedKey: eventKey });
+  }
 
-    render() {
-        return (
-                <Nav bsStyle="tabs">
-                    <LinkContainer to={"/home"}>
-                        <NavItem> Home </NavItem>
-                    </LinkContainer>
+  render() {
+    return (
+      <Nav bsStyle="tabs">
+        <LinkContainer to="/home">
+          <NavItem> Home </NavItem>
+        </LinkContainer>
 
-                    <LinkContainer to={"/services"} >
-                        <NavItem >Serviços</NavItem>
-                    </LinkContainer>
+        <LinkContainer to="/services" >
+          <NavItem >Serviços</NavItem>
+        </LinkContainer>
 
-                    <LinkContainer to="/products">
-                        <NavItem >Produtos</NavItem>
-                    </LinkContainer>
+        <LinkContainer to="/products">
+          <NavItem >Produtos</NavItem>
+        </LinkContainer>
 
-                    <LinkContainer to="/contacts">
-                        <NavItem >Contato</NavItem>
-                    </LinkContainer>
+        <LinkContainer to="/contacts">
+          <NavItem >Contato</NavItem>
+        </LinkContainer>
 
-                    <LinkContainer to="/calendar">
-                        <NavItem >Calendário</NavItem>
-                    </LinkContainer>
+        <LinkContainer to="/calendar">
+          <NavItem >Calendário</NavItem>
+        </LinkContainer>
 
-                    <LinkContainer to="/about">
-                        <NavItem >Sobre</NavItem>
-                    </LinkContainer>
+        <LinkContainer to="/about">
+          <NavItem >Sobre</NavItem>
+        </LinkContainer>
 
-                    <LinkContainer to="/login">
-                        <NavItem >Login</NavItem>
-                    </LinkContainer>
-                </Nav>
-        );
-    }
+        <LinkContainer to="/login">
+          <NavItem >Login</NavItem>
+        </LinkContainer>
+
+        <LinkContainer to="/cart">
+          <NavItem >Cart</NavItem>
+        </LinkContainer>
+      </Nav>
+    );
+  }
 }
