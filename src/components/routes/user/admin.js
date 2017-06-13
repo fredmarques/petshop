dd8927dc836e7553e6329797f209fd55f78ee2e1
Frederico Marques <frederico.marques.22@gmail.com>
2017-06-13 00:02:08 -0300

    cleanup customer and admin containers

--- src/components/routes/user/admin.js
+++ src/components/routes/user/admin.js
@@ -1,28 +1,10 @@
 import './User.css';
 import React, { Component } from 'react';
-import { Field, reduxForm } from 'redux-form';
-import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 // import { loginAdmin, loginUser } from '../../../actions';
 import { loginStatus, loginMode } from '../../../reducers/login';
 
 class Admin extends Component {
-    renderField(field) {
-        const { meta: { touched, error } } = field;
-        const className = '';
-        return (
-            <div className={className}>
-                <input className="form-control"
-                    type={field.type}
-                    placeholder={field.placeholder}
-                    {...field.input} />
-                <div className="text-help">
-                    {touched ? error : ''}
-                </div>
-            </div>
-        );
-    }
-
     onSubmit(values) {
     }
     render() {
@@ -40,8 +22,4 @@ const mapStateToProps = (state) => {
     }
 };
 
-export default reduxForm({
-    form: 'Admin'
-})(
-    connect(mapStateToProps)(Admin)
-    );
+export default connect(mapStateToProps)(Admin)
--- src/components/routes/user/customer.js
+++ src/components/routes/user/customer.js
@@ -1,44 +1,16 @@
 import './User.css';
 import React, { Component } from 'react';
-import { Field, reduxForm } from 'redux-form';
-import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { loginAdmin, loginUser } from '../../../actions';
+// import { loginAdmin, loginUser } from '../../../actions';
 import { loginStatus, loginMode } from '../../../reducers/login';
 
 class Customer extends Component {
-    renderField(field) {
-        const { meta: { touched, error } } = field;
-        const className = '';
-        return (
-            <div className={className}>
-                <input className="form-control"
-                    type={field.type}
-                    placeholder={field.placeholder}
-                    {...field.input} />
-                <div className="text-help">
-                    {touched ? error : ''}
-                </div>
-            </div>
-        );
-    }
 
-    onSubmit(values) {
-        if (values.email === 'foo@admin.com') {
-            this.props.loginAdmin();
-        } else {
-            this.props.loginUser();
-        }
-        values.email = '';
-        values.password = '';
-    }
     render() {
-        const { handleSubmit } = this.props;
         return (
             <div> area do cliente </div>
         );
     }
-
 }
 
 const mapStateToProps = (state) => {
@@ -48,8 +20,4 @@ const mapStateToProps = (state) => {
     }
 };
 
-export default reduxForm({
-    form: 'Login'
-})(
-    connect(mapStateToProps, { loginAdmin, loginUser })(Customer)
-);
+export default connect(mapStateToProps)(Customer);
