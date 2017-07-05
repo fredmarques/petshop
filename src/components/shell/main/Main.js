2718624a896db45301090d5584741f38a5349f76
Frederico Marques <frederico.marques.22@gmail.com>
2017-07-05 13:11:47 -0300

    remove browserhistory from main

--- src/components/shell/main/Main.js
+++ src/components/shell/main/Main.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
 import Content from '../content/Content';
 import Navigation from '../navigation/Navigation';
-import { BrowserRouter}  from 'react-router-dom';
+// import { BrowserRouter}  from 'react-router-dom';
 class Main extends Component {
   render() {
     return (
-      <BrowserRouter>
         <div className="mainContent">
           <Navigation />
           <Content/>
         </div>
-      </BrowserRouter> 
     );
   }
 }
