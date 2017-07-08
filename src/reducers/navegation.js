18cc2aeba545bc1f324c32a5ddce5c1e0666fb45
Frederico Marques <frederico.marques.22@gmail.com>
2017-07-08 17:14:43 -0300

    make navegation reactive

--- src/reducers/navegation.js
+++ src/reducers/navegation.js
@@ -1,39 +1,53 @@
+import {LOGIN_AS_ADMIN, LOGIN_AS_USER}  from '../constants/ActionTypes'
 const initalNavList = [
-          {
-            path: '/home',
-            label: 'Home'
-          },
-          {
-            path: '/services',
-            label: 'Serviços'
-          },
-          {
-            path: '/products',
-            label: 'Produtos'
-          },
-          {
-            path: '/contact',
-            label: 'Contato'
-          },
-          {
-            path: '/calendar',
-            label: 'Calendário'
-          },
-          {
-            path: '/user',
-            label: 'Usuario'
-          },
-          {
-            path: '/cart',
-            label: 'Carrinho'
-          },
-          {
-            path: '/pet',
-            label: 'Meus pets'
-          }
-        ]
+  {
+    path: '/home',
+    label: 'Home'
+  },
+  {
+    path: '/services',
+    label: 'Serviços'
+  },
+  {
+    path: '/products',
+    label: 'Produtos'
+  },
+  {
+    path: '/contact',
+    label: 'Contato'
+  },
+  {
+    path: '/calendar',
+    label: 'Calendário'
+  },
+  {
+    path: '/user',
+    label: 'Usuario'
+  }
+]
+
+const userList = [
+  ...initalNavList,
+  {
+    path: '/cart',
+    label: 'Carrinho'
+  },
+  {
+    path: '/pet',
+    label: 'Meus pets'
+  }
+]
+
+const adminList = [
+  ...initalNavList,
+]
+
 export default function navegation(state = initalNavList, action) {
   switch (action.type) {
+    case LOGIN_AS_USER:
+      return userList
+    case LOGIN_AS_ADMIN:
+     return adminList 
     default:
       return state;
   }
