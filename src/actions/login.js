a7f393c4e247687deb7229aacd4e06ebe39f5b4f
Frederico Marques <frederico.marques.22@gmail.com>
2017-07-09 15:55:14 -0300

    fetch user info when login on page

--- src/actions/login.js
+++ src/actions/login.js
@@ -3,27 +3,46 @@ import { registerUser, login, getInfo } from '../api/user'
 import setAuthToken from '../utils/setAuthToken'
 
 export const loginAdmin = (email, password) => (dispatch, getState) => {
-  return login(email, password).then(({data}) => {
+  return login(email, password).then(({ data }) => {
+
     localStorage.setItem('jwt', data.token)
-    return dispatch({
-      type: types.LOGIN_AS_ADMIN
+    setAuthToken(data.token)
+
+    return getInfo(data.token).then(({ data }) => {
+      const { user } = data
+      return dispatch({
+        type: types.LOGIN_AS_ADMIN, user
+      })
+    }).catch(err => {
+      console.log(err)
+      alert('Algo de errado com o token de sessão')
     })
- }).catch(err => {
+
+  }).catch(err => {
     console.log(err);
     alert('Usuário não encontrado')
   })
 }
 
 export const loginUser = (email, password) => (dispatch, getState) => {
-  return login(email, password).then(({data}) => {
+  return login(email, password).then(({ data }) => {
+
     localStorage.setItem('jwt', data.token)
     setAuthToken(data.token)
-    return dispatch({
-      type: types.LOGIN_AS_USER
+
+    return getInfo(data.token).then(({ data }) => {
+      const { user } = data
+      return dispatch({
+        type: types.LOGIN_AS_USER, user
+      })
+    }).catch(err => {
+      console.log(err)
+      alert('Algo de errado com o token de sessão')
     })
+
   }).catch(err => {
     console.log(err);
-    alert('usuario não encontrado')
+    alert('Usuário não encontrado')
   })
 }
 
@@ -47,14 +66,14 @@ export const localLogin = () => (dispatch, getState) => {
   const token = localStorage.getItem('jwt');
   if (token) {
     setAuthToken(token)
-    return getInfo(token).then(({data}) => {
+    return getInfo(token).then(({ data }) => {
       const { user } = data
       if (user.type === 'admin' || user.id === 'admin') {
         return dispatch({ type: types.LOGIN_AS_ADMIN, user })
       } else {
         return dispatch({ type: types.LOGIN_AS_USER, user })
       }
-    }).catch(err =>{
+    }).catch(err => {
       console.log(err)
       alert('deu ruim no login com token')
     })
