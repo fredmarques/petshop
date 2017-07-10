4c1574f657f5ced6b5da9f1e8b62a83ba719971c
Frederico Marques <frederico.marques.22@gmail.com>
2017-07-09 23:07:40 -0300

    on cart: add card number to state

--- src/components/routes/cart/Cart.js
+++ src/components/routes/cart/Cart.js
@@ -7,7 +7,8 @@ class Cart extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      products: this.props.products
+      products: this.props.products,
+      card: ''
     }
     // this.products = this.props.products;
     this.total = this.props.total;
@@ -15,15 +16,23 @@ class Cart extends Component {
     this.onClearCartClick = this.props.onClearCartClick;
     this.checkout = this.checkout.bind(this);
     this.clear = this.clear.bind(this);
+    this.updateInputValue = this.updateInputValue.bind(this);
   }
 
   checkout(){
+    console.log(this.state.card)
     this.onCheckoutClick()
     this.setState({product: []})
   }
   clear(){
     this.onClearCartClick();
     this.setState({product: []})
+  }
+
+  updateInputValue(e) {
+    this.setState({
+      card: e.target.value
+    });
   }
   render() {
     return (
@@ -35,7 +44,7 @@ class Cart extends Component {
               </Col>
               <Col xs={12}>
                 <br/>
-                <input name="creditcardcart" id="creditcardcart" type="text" placeholder="Cartão de crédito"/>
+                <input value={this.state.card} onChange={this.updateInputValue} name="creditcardcart" id="creditcardcart" type="text" placeholder="Cartão de crédito"/>
                 <br/><br/>
                 <Button bsStyle="success" onClick={this.checkout}>Finalizar compra</Button>
                 <Button bsStyle="danger" onClick={this.clear}>Limpar carrinho</Button>
